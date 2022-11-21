03892d351bce9da68f2045276fdd6ba1164769cf
CarlosMontesdeoca <mateo.game@hotmail.com>
2022-11-21 14:02:25 -0500

    selected project to migrate

--- SendProject.py
+++ SendProject.py
@@ -31,12 +31,21 @@ cursormysql = MySQLConnection.cursor()
 cursormysql.execute(f"SELECT CO.id, C.nom, C.id, S.ciu, S.dir, CO.email, CO.telf, CO.nom, C.ind, S.prov FROM contacts CO JOIN plants S ON CO.plant_id = S.id JOIN clients C ON S.client_id = C.id WHERE CO.id LIKE {client}")
 clientInfo = cursormysql.fetchone()
 
-# busca el cliente en SisMetPrec
+# busca el cliente en SisMetPrec y crealo si no existe
 querryFindClient = f"IF NOT EXISTS ( SELECT * FROM Clientes WHERE codAux LIKE {client}) BEGIN INSERT INTO Clientes (codAux,  NomCli, CiRucCli, CiuCli, DirCli, EmaCli, TelCli, ConCli, EstCli, matProCli) VALUES ({clientInfo[0]}, '{clientInfo[1]}', '{clientInfo[2]}', '{clientInfo[3]}', '{clientInfo[4]}', '{clientInfo[5]}', '{clientInfo[6]}', '{clientInfo[7]}', 'A', '{clientInfo[8]}' ) END"
 cursorsqlsrv.execute(querryFindClient)
 SQLServerConnection.commit() 
 
-##crea el registro en Identificadores 
+#Guarda el codigo del Cliente
+cursorsqlsrv.execute(f"SELECT CodCli FROM Clientes WHERE codAux LIKE {client}")
+CodCli = cursorsqlsrv.fetchone()
+
+
+##Obtener la informacion de el Proyecto
+
+cursormysql.execute(f"SELECT * FROM projects WHERE codPro LIKE {codPro}") 
+project = cursormysql.fetchone()
+print(project)
 # qeurryIdentificadores = f"INSERT INTO Identificadores(codcli, idepro) VALUES (CODCLI, {project[0]})"
 
 
