8883f845abf112fae3c468cdae88f569feadeccb
CarlosMontesdeoca <mateo.game@hotmail.com>
2023-11-17 10:32:13 -0500

    datos de ordenes

--- MY_scripts/MigracionCalibraciones.py
+++ MY_scripts/MigracionCalibraciones.py
@@ -1,31 +1,15 @@
 # CONEXION pyw 
 ## este escript esta creado para comuicar dos bases de datos que se actualizaran cuando un metrolog envie datos primarios de la tablet al servidor
-import pyodbc
 import pymysql
 import numpy as np
 from datetime import date
 
 today = date.today()
 
-server1='tcp:192.168.9.221'
-dbname1='SisMetPrec'
-user1='sa'
-password1='Sistemas123*'
-
-logs = ''
-
 try:
-    SQLServerConnection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL server}; SERVER='+server1+';DATABASE='+dbname1+';UID='+user1+';PWD='+password1)
-    print (" ==> CONNECCTION SUCCESS WITH SQL SERVER")
-except:
-    logs += "==> error to try connect the database SQL Server \n" 
-    print ('error to try connect the database SQL Server')
-
-try:
-    MySQLConnection = pymysql.connect(host="192.168.9.221",user="root",passwd="AdminSistemas@",database="metrologia" )
+    MySQLConnection = pymysql.connect(host="127.0.0.1",user="root",database="pruebas" )
     print (" ==> CONNECCTION SUCCESS WITH MYSQL")
 except:
-    logs += "==> error to try connect the database MySQL \n" 
     print ('error to try connect the database MySQL')
 
 
@@ -33,6 +17,30 @@ print('=========================================================================
 # CONSULTAS
 
 ## CUSORES DE CONSULTAS 
-cursorsqlsrv = SQLServerConnection.cursor()
 cursormysql = MySQLConnection.cursor()
 ## ==============================================================
+
+print('READING PROJECTS DATA....')
+## busca todos los proyectos pendientes de MySQL
+cursormysql.execute("SELECT * FROM projects WHERE codPro IS NOT NULL")
+data = cursormysql.fetchall()
+
+print(len(data))
+i = 1
+# print(data[0])
+for dt in data:
+    aux_id = dt[0]
+    aux_est = 'P'
+    if ( dt[1] == 'N/A'): 
+        offert = 'AUX-NAC20##-' + str(i).zfill(3)
+        i += 1
+    else:
+        offert = dt[1]
+    if ( dt[8] == 'F' ):
+        aux_est = 'F'
+    
+    print(f"CREATING ORDER ================> {offert}")
+    query_offert = f"INSERT INTO orders (N_offert, plant_id, contact_id, ases_id, fact, itms, est, com, created_at, updated_at) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    valor_offet = (offert,dt[4],dt[5],dt[11],dt[9],dt[14],aux_est,dt[17],dt[18],dt[19])
+    
+MySQLConnection.close()
