b99d3c5e72db0931769feaf7778e76fb751d49ab
CarlosMontesdeoca <mateo.game@hotmail.com>
2023-11-22 10:16:31 -0500

    prueba de migracion 1

--- MY_scripts/MigracionCalibraciones.py
+++ MY_scripts/MigracionCalibraciones.py
@@ -34,7 +34,7 @@ for dt in data:
     aux_id = dt[0]
     aux_est = 'P'
     if ( dt[1] == 'N/A'): 
-        offert = 'AUX-NAC20##-' + str(i).zfill(3)
+        offert = 'AUX-NAC20##-' + str(i).zfill(3) + '.'
         i += 1
     else:
         offert = dt[1]
@@ -49,23 +49,49 @@ for dt in data:
 
     # Se compara que el numero de orden no este registrado
     # si esta registrado solo obtendremos el id de orden o lo crearemos para agregar los proyectos
+    if dt[11] :
+        ases = dt[11]
+    else :
+        ases = 1
     if order :
         order_id = order[0]
     else :
         print('   CREANDO PEDIDO..')
-        query_offert = f"INSERT INTO orders (N_offert, plant_id, contact_id, ases_id, fact, itms, est, com, created_at, updated_at) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        valor_offet = (offert,dt[4],dt[5],dt[11],dt[9],dt[14],aux_est,dt[17],dt[18],dt[19])
-        # cursormysql.execute(query_offert, valor_offet)
-        # MySQLConnection.commit()
-        # order = cursormysql.fetchone()
-        # order_id = order[0]
+        print(dt)
+        query_offert = f"""INSERT INTO orders (N_offert, plant_id, contact_id, ases_id, fact, itms, est, created_at, updated_at) VALUES ('{offert}',{dt[4]},{dt[5]},{ases},'{dt[9]}','{dt[14]}','{aux_est}','{dt[18]}','{dt[19]}')"""
+        try:
+            print(query_offert)
+            cursormysql.execute(query_offert)
+            MySQLConnection.commit()
+            
+            cursormysql.execute(f"SELECT * FROM orders WHERE N_offert LIKE '{offert}'")
+            order = cursormysql.fetchone()
+            order_id = order[0]
+            print(f"       PEDIDO CREADO CORRECTAMENTE")
+        except pymysql.connect.Error as err:
+            print(f"Error de MySQL: {err}")
 
     # Crear proyecto y asignarlo al numero de orden
     print('   CREANDO PROYECTO..')
-    query_calibrate = f"INSERT INTO calibrates (order_id, tip, codPro, metrologist_id, recPor, fecAsg, path, est, com, created_at, updated_at) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    valor_calibrate = (order_id,dt[2],dt[3],dt[6],dt[7],dt[12],dt[15],dt[8],dt[-3],dt[-2],dt[-1])
-
-
+    query_calibrate = f"""INSERT INTO calibrates (order_id, tip, codPro, metrologist_id, recPor, fecAsg, path, est, com, created_at, updated_at) VALUES (
+    {order_id},'{dt[2]}','{dt[3]}',{dt[6]},'{dt[7]}','{dt[12]}','{dt[15]}','{dt[8]}','{dt[17]}','{dt[-2]}','{dt[-1]})'"""
+    cursormysql.execute(query_calibrate)
+    MySQLConnection.commit()
+    project = cursormysql.fetchone()
+    project_id = project[0]
+    
+    print('   INDEXANDO CERTIFICADOS..')
+    if dt[2] == 'ICC':
+        query_certificates = f"UPDATE certificates SET calibrate_id = {project_id} WHERE project_id LIKE {dt[0]}"
+        cursormysql.execute(query_certificates)
+        MySQLConnection.commit()
+    else :
+        print('       SIN CERTIFICADOS PARA ICM')
+    
+    print('   INDEXANDO EVENTOS..')
+    query_event = f"UPDATE calendar SET calibrate_id = {project_id} WHERE project_id LIKE {dt[0]}"
+    cursormysql.execute(query_event)
+    MySQLConnection.commit()
     # try: 
     #     cursormysql.execute(f"INSERT INTO balances (tip,marc,modl,ser,cls,maxCap,usCap,div_e,div_d,uni,plant_id) VALUES ('{data_cert[3]}','{data_cert[5]}','{data_cert[6]}','{data_cert[7]}','{clsBl}',{data_cert[8]},{data_cert[9]},{round(data_cert[10],6)},{round(data_cert[11],6)},'kg',{idPlant[0]})")
     #     MySQLConnection.commit()
